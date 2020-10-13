6d721dfe7181cf877f3dbae97cf1f37ed6ad5a2b
travisbrigman <travis1000@gmail.com>
2020-10-13 14:44:16 -0500

    initial commit

---
+++ animals.py
@@ -0,0 +1,129 @@
+from datetime import date
+
+class Llama:
+    def __init__(self, name, species, date, walking):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = walking
+
+class Capybara:
+    def __init__(self, name, species, date, walking):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = walking
+
+class Tortoise:
+    def __init__(self, name, species, date, walking):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = walking
+
+class Pig:
+    def __init__(self, name, species, date, walking):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = walking
+
+class Ermine:
+    def __init__(self, name, species, date, walking):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = walking
+
+
+#walking🦙
+june = Llama("June","Llame",date, True)
+johnny = Capybara("Johnny","Capybara",date, True)
+raphael = Tortoise("Raphael","Tortoise",date, True)
+wilbur = Pig("Wilbur","Pig",date, True)
+ernesto = Ermine("Ernesto","Ermine",date, True)
+
+class Garden_Snake:
+    def __init__(self, name, species, date, slithering):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = slithering   
+
+class Earthworm:
+    def __init__(self, name, species, date, slithering):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = slithering   
+
+class Salamander:
+    def __init__(self, name, species, date, slithering):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = slithering   
+
+class Python_Snake:
+    def __init__(self, name, species, date, slithering):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = slithering   
+
+class Boa_Snake:
+    def __init__(self, name, species, date, slithering):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = slithering 
+
+#slithering🐍
+sam = Garden_Snake("Sam", "Garden Snake", date, True)  
+errol = Earthworm("Errol", "Earthworm", date, True)  
+sal = Salamander("Sal", "Salamander", date, True)  
+pippin = Python_Snake("Pippin", "Python Snake", date, True)  
+bruce = Boa_Snake("Bruce", "Boa Snake", date, True)  
+
+class Fish:
+    def __init__(self, name, species, date, swimming):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = swimming 
+
+class Alligator:
+    def __init__(self, name, species, date, swimming):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = swimming 
+
+class Seahorse:
+    def __init__(self, name, species, date, swimming):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = swimming 
+
+class Frog:
+    def __init__(self, name, species, date, swimming):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = swimming 
+
+class Platypus:
+    def __init__(self, name, species, date, swimming):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = swimming 
+
+
+#swimming🐠
+nemo = Fish("Nemo", "Fish", date, True)
+alan = Alligator("Alan", "Alligator", date, True)
+cedric = Seahorse("Cedric", "Seahorse", date, True)
+fred = Frog("Fred", "Frog", date, True)
+pedro = Platypus("Pedro", "Platypus", date, True)
